90976df38987d789f551cf58684664467a635ae9
Gokul Krishna <gkrishna3@sapient.com>
2016-11-30 21:59:35 +0530

    GREEN
    Added more testcase

--- src/test/com/kata/CheckoutTest.java
+++ src/test/com/kata/CheckoutTest.java
@@ -11,16 +11,23 @@ import junit.framework.TestCase;
 
 public class CheckoutTest extends TestCase {
 	@Test
-	public void price_of_one_item_two_Quantity() {
+	public void price_of_zero_item() {
 
 		Checkout checkoutItem = new Checkout();
-		assertEquals(checkoutItem.getTotalPriceByItemAndQuantity("A", 1, 50) == 0);
+		assertEquals(checkoutItem.getTotalPriceByItemAndQuantity(" ", 1, 50), 0);
 	}
 	
 	@Test
 	public void price_of_one_item_one_Quantity() {
 
 		Checkout checkoutItem = new Checkout();
-		assertTrue(checkoutItem.getTotalPriceByItemAndQuantity("A", 2, 50) == 100);
+		assertEquals(checkoutItem.getTotalPriceByItemAndQuantity("A", 1, 50), 50);
+	}
+	
+	@Test
+	public void price_of_one_item_two_Quantity() {
+
+		Checkout checkoutItem = new Checkout();
+		assertEquals(checkoutItem.getTotalPriceByItemAndQuantity("B", 2, 30), 60);
 	}
 }
