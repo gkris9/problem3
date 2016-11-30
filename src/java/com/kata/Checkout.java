f01cf6dac0c8e8cdeaca8013351d3637fa4ef89f
Gokul Krishna <gkrishna3@sapient.com>
2016-11-30 22:05:27 +0530

    Refactor
    Modified return type from Integer to int

--- src/java/com/kata/Checkout.java
+++ src/java/com/kata/Checkout.java
@@ -1,7 +1,7 @@
 package java.com.kata;
 
 public class Checkout {
-	public Integer getTotalPriceByItemAndQuantity(String product, int Quantity, int price) {
+	public int getTotalPriceByItemAndQuantity(String product, int Quantity, int price) {
 
 		Integer productPrice = 0;
 		if (product.equals("A") || product.equals("B")) {
