7d772b645b0d96a4853e6433d8ce6885627e1239
Gokul Krishna <gkrishna3@sapient.com>
2016-11-30 22:06:49 +0530

    GREEN
    Created object outside the method

--- src/test/com/kata/CheckoutTest.java
+++ src/test/com/kata/CheckoutTest.java
@@ -10,24 +10,23 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 public class CheckoutTest extends TestCase {
+	Checkout checkoutItem = new Checkout();
+
 	@Test
 	public void price_of_zero_item() {
 
-		Checkout checkoutItem = new Checkout();
 		assertEquals(checkoutItem.getTotalPriceByItemAndQuantity(" ", 1, 50), 0);
 	}
-	
+
 	@Test
 	public void price_of_one_item_one_Quantity() {
 
-		Checkout checkoutItem = new Checkout();
 		assertEquals(checkoutItem.getTotalPriceByItemAndQuantity("A", 1, 50), 50);
 	}
-	
+
 	@Test
 	public void price_of_one_item_two_Quantity() {
 
-		Checkout checkoutItem = new Checkout();
 		assertEquals(checkoutItem.getTotalPriceByItemAndQuantity("B", 2, 30), 60);
 	}
 }
