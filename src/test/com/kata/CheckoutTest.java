f0e9f98696e7d6a738fddf1eb04e1e8bbc9eef25
Gokul Krishna <gkrishna3@sapient.com>
2016-11-30 19:47:17 +0530

    GREEN
    Added Test Case for 1 item

---
+++ src/test/com/kata/CheckoutTest.java
@@ -0,0 +1,25 @@
+package test.com.kata;
+
+import static org.junit.Assert.*;
+
+import java.com.kata.Checkout;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class CheckoutTest extends TestCase {
+	@Test
+	public void price_of_one_item_two_Quantity() {
+
+		Checkout checkoutItem = new Checkout();
+		assertFalse(checkoutItem.getTotalPriceByItemAndQuantity("A", 1, 50) == 0);
+	}
+	
+	@Test
+	public void price_of_one_item_one_Quantity() {
+
+		Checkout checkoutItem = new Checkout();
+		assertTrue(checkoutItem.getTotalPriceByItemAndQuantity("A", 2, 50) == 100);
+	}
+}
