95e10cce2adf98ad50112ad4deec89cfd6146aaa
Gokul Krishna <gkrishna3@sapient.com>
2016-11-30 21:55:10 +0530

    GREEN
    Added product list

--- src/java/com/kata/Checkout.java
+++ src/java/com/kata/Checkout.java
@@ -4,7 +4,7 @@ public class Checkout {
 	public Integer getTotalPriceByItemAndQuantity(String product, int Quantity, int price) {
 
 		Integer productPrice = 0;
-		if (product.equals("A")) {
+		if (product.equals("A") || product.equals("B")) {
 			return productPrice = Quantity * price;
 		}
 
